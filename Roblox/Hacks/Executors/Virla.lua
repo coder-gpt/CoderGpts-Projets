761793d964450ac175262f736a0e4dc415992b69
coder-gpt <stickbricksstickbricks030@gmail.com>
2026-05-06 20:55:28 +0800

    Update and rename Virla.lua to Virly.lua

--- Roblox/Hacks/Executors/Virla.lua
+++
@@ -1,5 +0,0 @@
---- Hey There, are you lost?
---- Put this into ur executor
-loadstring(game:HttpGet("https://raw.githubusercontent.com/coder-gpt/CoderGpts-Projets/refs/heads/main/Roblox/Hacks/Executors/VirlaLoader.lua"))()
-
---- looking for source code? its obfuscated
---
+++ Roblox/Hacks/Executors/Virly.lua
@@ -0,0 +1,5 @@
+--- Hey There, are you lost?
+--- Put this into ur executor
+loadstring(game:HttpGet("https://raw.githubusercontent.com/coder-gpt/CoderGpts-Projets/refs/heads/main/Roblox/Hacks/Executors/VirlyLoader.lua"))()
+
+--- looking for source code? its obfuscated
