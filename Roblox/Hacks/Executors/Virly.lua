b2cd849887508d2d542748d876746ad00ce4ec63
coder-gpt <stickbricksstickbricks030@gmail.com>
2026-05-06 21:14:28 +0800

    Update Virly.lua

--- Roblox/Hacks/Executors/Virly.lua
+++ Roblox/Hacks/Executors/Virly.lua
@@ -3,3 +3,14 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/coder-gpt/CoderGpts-Projets/refs/heads/main/Roblox/Hacks/Executors/VirlyLoader.lua"))()
 
 --- looking for source code? its obfuscated
+
+
+
+
+
+
+
+
+--- VERSION CHECK SYSTEM
+
+Local Version = 1.3
