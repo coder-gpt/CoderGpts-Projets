1d724ab636c50c60cb0397006b2b03ffe18e8364
coder-gpt <stickbricksstickbricks030@gmail.com>
2026-05-06 22:33:59 +0800

    Update Virly.lua

--- Roblox/Hacks/Executors/Virly.lua
+++ Roblox/Hacks/Executors/Virly.lua
@@ -1,5 +1,5 @@
 --- Hey There, are you lost?
 --- Put this into ur executor
-loadstring(game:HttpGet("https://raw.githubusercontent.com/coder-gpt/CoderGpts-Projets/refs/heads/main/Roblox/Hacks/Executors/VirlyLoader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/coder-gpt/CoderGpts-Projets/refs/heads/main/Roblox/Hacks/Executors/Loaders/VirlyLoader.lua"))()
 
 --- looking for source code? its obfuscated
